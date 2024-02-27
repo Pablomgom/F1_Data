8910258105a15786537282dde34ba0d2601c5daa
Pablomgom <pablo_margo@hotmail.com>
2024-02-27 13:08:12 +0100

    Off season

--- src/analysis/data_analysis.py
+++ src/analysis/data_analysis.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from src.utils.utils import get_quartiles, find_nearest_non_repeating
-from src.variables.team_colors import team_colors_2023
+from src.variables.team_colors import team_colors_2023, team_colors
 
 
 def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, save_data=False):
@@ -78,10 +78,6 @@ def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, 
         length = max(telemetry_data[0]['Distance'])
         corners_per_meter = length / corners
         all_speeds = [max(telemetry['Speed']) for telemetry in telemetry_data]
-        all_q1 = [get_quartiles(sorted(telemetry['Speed']))[0] for telemetry in telemetry_data]
-        all_medians = [get_quartiles(sorted(telemetry['Speed']))[1] for telemetry in telemetry_data]
-        all_q3 = [get_quartiles(sorted(telemetry['Speed']))[2] for telemetry in telemetry_data]
-        all_avg = [get_quartiles(sorted(telemetry['Speed']))[3] for telemetry in telemetry_data]
         all_full_gas = [len(telemetry[telemetry['Throttle'].isin([100, 99])]) / len(telemetry) for telemetry in
                         telemetry_data]
         all_lifting = [len(telemetry[(telemetry['Throttle'] >= 1) & (telemetry['Throttle'] <= 99)]) / len(telemetry) for
@@ -93,17 +89,8 @@ def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, 
         avg_corner_speed = get_quartiles(speed_in_corners)[3]
 
         median_max_speed = np.median(all_speeds)
-        median_q1 = np.median(all_q1)
-        median_median = np.median(all_medians)
-        median_q3 = np.median(all_q3)
-        median_avg = np.median(all_avg)
         median_full_gas = np.median(all_full_gas)
         median_lifting = np.median(all_lifting)
-
-        # data.append(
-        #     [corners_per_meter, median_max_speed, median_q1, median_median,
-        #      median_q3, median_avg, median_full_gas, median_lifting,
-        #      q1_corner_speed, median_corner_speed, q3_corner_speed, avg_corner_speed])
 
         data.append(
             [corners_per_meter, median_max_speed, median_full_gas, median_lifting,
@@ -123,13 +110,12 @@ def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, 
 
     if clusters is None:
         inertia = []
-        cluster_range = range(1, 11)  # Adjust the range as needed
+        cluster_range = range(1, 11)
         for clusters in cluster_range:
             kmeans = KMeans(n_clusters=clusters, init='k-means++', random_state=42)
             kmeans.fit(principal_components)
             inertia.append(kmeans.inertia_)
 
-        # Plotting the results
         plt.figure(figsize=(10, 6))
         plt.plot(cluster_range, inertia, marker='o')
         plt.title('Elbow Method For Optimal k')
@@ -148,25 +134,12 @@ def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, 
     fig, ax = plt.subplots(figsize=(8, 8), dpi=150)
 
     texts = []
-    colors = ['red', 'blue', 'green', 'purple']  # Assuming 3 clusters; extend this list if there are more clusters
-
     for i, name in enumerate(circuits):
         ax.scatter(principal_components[i, 0], principal_components[i, 1], color='#00BFFF', s=100)
         texts.append(ax.text(principal_components[i, 0], principal_components[i, 1], name,
                              font='Fira Sans', fontsize=11.5))
 
-    # for i, center in enumerate(pca.transform(kmeans.cluster_centers_)):
-    #     ax.scatter(center[0], center[1], s=300, c='#FF8C00', zorder=-100)
-
     adjust_text(texts, autoalign='xy', ha='right', va='bottom', only_move={'points': 'y', 'text': 'xy'})
-
-    legend_lines = [Line2D([0], [0], color='blue', lw=4),
-                    Line2D([0], [0], color='red', lw=4),
-                    Line2D([0], [0], color='green', lw=4)]
-
-    # plt.legend(legend_lines, ['High speed tracks', 'Medium speed tracks', 'Low speed tracks'],
-    #            loc='lower center', fontsize='large')
-
     ax.axis('off')
     ax.grid(False)
     plt.title('SIMILARITY BETWEEN CIRCUITS', font='Fira Sans', fontsize=28)
@@ -177,23 +150,41 @@ def cluster_circuits(year, rounds, prev_year=None, circuit=None, clusters=None, 
 
 def predict_race_pace(year=2024, track='Bahrain', session='R'):
 
+    track_features = ['Corner Density', 'Top Speed', 'Full Gas', 'Lifting', 'Q1 Corner Speed', 'Median Corner Speed',
+                    'Q3 Corner Speed', 'Average Corner Speed', 'Corner Speed Variance']
+
+    teams = ['Red Bull Racing', 'Aston Martin', 'Alpine', 'Mercedes', 'RB',
+             'Kick Sauber', 'Williams', 'Haas F1 Team', 'McLaren', 'Ferrari']
     if session == 'R':
         race_pace_delta = pd.read_csv('../resources/csv/Race_pace_delta.csv')
     else:
         race_pace_delta = pd.read_csv('../resources/csv/Qualy_pace_delta.csv')
     circuit_data = pd.read_csv('../resources/csv/Circuit_data.csv')
-    race_pace_delta = race_pace_delta.pivot(index=['Year', 'Track', 'Session'], columns=['Team'], values='Delta').reset_index()
-    teams = ['Red Bull Racing', 'Aston Martin', 'Alpine', 'Mercedes', 'AlphaTauri',
-             'Alfa Romeo', 'Williams', 'Haas F1 Team', 'McLaren', 'Ferrari']
-    predict_data = pd.merge(race_pace_delta, circuit_data, on=['Year', 'Track'], how='inner')
+
+    race_pace_delta['Team'] = race_pace_delta['Team'].replace({'AlphaTauri': 'RB',
+                                                               'Alfa Romeo': 'Kick Sauber'})
+
+    race_pace_delta.sort_values(by=['Year', 'Session'], inplace=True)
+    race_pace_delta['Recent_Avg_Delta'] = race_pace_delta.groupby('Team')['Delta'].transform(
+        lambda x: x.rolling(window=2, min_periods=1).mean())
+
+    race_pace_delta_pivot = race_pace_delta.pivot_table(index=['Year', 'Track', 'Session'], columns='Team',
+                                                        values=['Delta', 'Recent_Avg_Delta']).reset_index()
+    race_pace_delta_pivot.columns = [f'{i}_{j}' if j != '' else f'{i}' for i, j in race_pace_delta_pivot.columns]
+
+    predict_data = pd.merge(race_pace_delta_pivot, circuit_data, on=['Year', 'Track'], how='inner')
     predict_data = predict_data.drop(['Session_y'], axis=1).rename({'Session_x': 'Session'}, axis=1)
-    predict_data = predict_data.sort_values(by=['Year', 'Session'], ascending=[True, True])
+    predict_data = predict_data.sort_values(by=['Year', 'Session'], ascending=[True, True]).reset_index(drop=True)
     predict_data['ID'] = range(1, len(predict_data) + 1)
-    for column in teams:
-        median_value = predict_data[column].median()
-        predict_data[column].fillna(median_value, inplace=True)
-    X = predict_data.drop(columns=teams+['Session', 'Track'])
-    y = predict_data[teams]
+    for team in teams:
+        for metric in ['Delta', 'Recent_Avg_Delta']:
+            column_name = f'{metric}_{team}'
+            if column_name in predict_data.columns:
+                median_per_year = predict_data.groupby('Year')[column_name].transform('median')
+                predict_data[column_name] = predict_data[column_name].fillna(median_per_year)
+    feature_columns = ['Year', 'ID'] + track_features + [f'{metric}_{team}' for team in teams for metric in ['Recent_Avg_Delta']]
+    X = predict_data[feature_columns]
+    y = predict_data[[f'Delta_{team}' for team in teams]]
     scaler = StandardScaler()
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     X_scaled = scaler.fit_transform(X)
@@ -201,19 +192,16 @@ def predict_race_pace(year=2024, track='Bahrain', session='R'):
     prev_year_data = circuit_data[(circuit_data['Year'] == year - 1) & (circuit_data['Track'] == track)]
     if len(prev_year_data) == 0:
         raise Exception('No data for that year/track combination')
+
     next_race_data = {
         'Year': year,
-        'ID': predict_data['ID'].max() + 1,
-        'Corner Density': prev_year_data['Corner Density'].loc[0],
-        'Top Speed': prev_year_data['Top Speed'].loc[0],
-        'Full Gas': prev_year_data['Full Gas'].loc[0],
-        'Lifting': prev_year_data['Lifting'].loc[0],
-        'Q1 Corner Speed': prev_year_data['Q1 Corner Speed'].loc[0],
-        'Median Corner Speed': prev_year_data['Median Corner Speed'].loc[0],
-        'Q3 Corner Speed': prev_year_data['Q3 Corner Speed'].loc[0],
-        'Average Corner Speed': prev_year_data['Average Corner Speed'].loc[0],
-        'Corner Speed Variance': prev_year_data['Corner Speed Variance'].loc[0]
+        'ID': predict_data['ID'].max() + 1
     }
+    for feature in track_features:
+        next_race_data[feature] = prev_year_data[feature].iloc[0]
+
+    for t in teams:
+        next_race_data[f'Recent_Avg_Delta_{t}'] = predict_data[f'Recent_Avg_Delta_{t}'].values[-1]
 
     next_race_df = pd.DataFrame([next_race_data])
     next_race_data_scaled = scaler.transform(next_race_df)
@@ -230,7 +218,7 @@ def predict_race_pace(year=2024, track='Bahrain', session='R'):
         delta_time = ref_session.laps.pick_team(fastest_team).pick_fastest()['LapTime']
 
     fig, ax = plt.subplots(figsize=(10, 8))
-    colors = [team_colors_2023.get(t) for t in next_race_prediction.index]
+    colors = [team_colors.get(year)[t] for t in next_race_prediction.index]
     bars = plt.barh(next_race_prediction.index, next_race_prediction[0].values, color=colors)
     for bar in bars:
         width = bar.get_width()
